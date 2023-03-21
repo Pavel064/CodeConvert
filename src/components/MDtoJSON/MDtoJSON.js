3e25a417a4055592b5877f236e0c67a8314d7fb4
Pavel <braurs64@gmail.com>
2023-03-21 22:59:13 +0400

    feat: :sparkles: added MDtoJSON component and included react-json-view

--- src/components/MDtoJSON/MDtoJSON.js
+++ src/components/MDtoJSON/MDtoJSON.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 
-function MDtoJSON() {
-  return <div>MDtoJSON</div>;
+function MDtoJSON(md) {
+  let _markdown = md.replaceAll('\u200B', '').split(/\n\s*\n\s*/);
+  const title = _markdown.shift().trim().slice(1);
+  let reference = _markdown.pop().trim().slice(1, -1);
+  if (reference === '') {
+    reference = _markdown.pop().trim().slice(1, -1);
+  }
+  const verseObjects = [];
+
+  for (let n = 0; n < _markdown.length / 2; n++) {
+    let urlImage;
+    let text;
+    if (/\(([^)]*)\)/g.test(_markdown[n * 2])) {
+      urlImage = /\(([^)]*)\)/g.exec(_markdown[n * 2])[1];
+      text = _markdown[n * 2 + 1];
+    } else {
+      text = _markdown[n * 2] + '\n' + _markdown[n * 2 + 1];
+    }
+    verseObjects.push({ urlImage, text, verse: (n + 1).toString() });
+  }
+
+  return { verseObjects, title, reference };
 }
+
+MDtoJSON.defaultProps = {
+  md: '',
+};
+
+MDtoJSON.propTypes = {
+  /** .MD file */
+  md: PropTypes.string,
+};
 
 export default MDtoJSON;
--- src/components/MDtoJSON/MDtoJSON.md
+++ src/components/MDtoJSON/MDtoJSON.md
@@ -1,7 +1,31 @@
-### Default example
+### An example of converting an MD file to a JSON object
 
 ```jsx
 import { MDtoJSON } from '@texttree/codeconvert-rcl';
+import React, { useState, useEffect } from 'react';
+import ReactJson from 'react-json-view';
+import axios from 'axios';
 
-<MDtoJSON />;
+function Component() {
+  const url =
+    'https://git.door43.org/ru_gl/ru_obs/raw/commit/e562a415f60c5262382ba936928f32479056310e/content/01.md';
+
+  const [jsonData, setJsonData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(url);
+      const jsonData = MDtoJSON(data);
+      setJsonData(jsonData);
+    };
+
+    fetchData();
+  }, []);
+
+  if (!jsonData) return <div>Loading...</div>;
+
+  return <ReactJson src={jsonData} />;
+}
+
+<Component />;
 ```
